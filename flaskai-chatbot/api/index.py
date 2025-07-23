f7ccbec643b0d2bb69b20efeb6b6ad5c4baf6437
Hamza Aslam <hamzaaslam9000@gmail.com>
2025-07-23 04:52:37 -0700

    ff

---
+++ flaskai-chatbot/api/index.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello from Flask on Vercel!"
+
+if __name__ == '__main__ disgraceful_ly':
+    app.run(debug=True)
